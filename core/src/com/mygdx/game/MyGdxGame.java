4f20a134b86fb2dc13776637abee5363060da8da
Clément Duval <cduv87@gmail.com>
2023-04-15 22:33:10 +0200

    stable

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
@@ -15,23 +17,39 @@ import java.util.Random;
 public class MyGdxGame extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private ShapeRenderer shapeRenderer;
+	private BitmapFont font;
+	private int scorePlayer1 = 0;
+	private int scorePlayer2 = 0;
 
 	private Rectangle paddle1, paddle2, ball;
 	private Vector2 ballVelocity;
 	private boolean ballActive = true;
 
+	private static final int GAME_HEIGHT = 480;
+	private static final int BUTTON_HEIGHT = 40;
+
+	private Rectangle resetButton, creditsButton, spaceButton, quitButton;
+
 	@Override
 	public void create() {
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+		camera.setToOrtho(false, 800, GAME_HEIGHT);
 
 		shapeRenderer = new ShapeRenderer();
 
-		paddle1 = new Rectangle(20, 240, 10, 60);
-		paddle2 = new Rectangle(770, 240, 10, 60);
+		font = new BitmapFont();
 
-		ball = new Rectangle(Gdx.graphics.getWidth() / 2 - 5, Gdx.graphics.getHeight() / 2 - 5, 10, 10);
+		paddle1 = new Rectangle(20, 160, 10, 60);
+		paddle2 = new Rectangle(770, 160, 10, 60);
+
+		ball = new Rectangle(Gdx.graphics.getWidth() / 2 - 5, GAME_HEIGHT / 2 - 5, 10, 10);
 		ballVelocity = getRandomVelocity(120);
+
+		resetButton = new Rectangle(0, 0, 200, BUTTON_HEIGHT);
+		creditsButton = new Rectangle(200, 0, 200, BUTTON_HEIGHT);
+		spaceButton = new Rectangle(400, 0, 200, BUTTON_HEIGHT);
+		quitButton = new Rectangle(600, 0, 200, BUTTON_HEIGHT);
+
 	}
 
 	@Override
@@ -45,18 +63,18 @@ public class MyGdxGame extends ApplicationAdapter {
 			ball.x += ballVelocity.x * Gdx.graphics.getDeltaTime();
 			ball.y += ballVelocity.y * Gdx.graphics.getDeltaTime();
 
-			if (ball.y <= 0 || ball.y + ball.height >= 480) {
+			if (ball.y <= BUTTON_HEIGHT +10 || ball.y + ball.height >= GAME_HEIGHT -10) {
+				System.out.println("ball.y" + ball.y);
 				ballVelocity.y = -ballVelocity.y;
 			}
 
 			if (ball.overlaps(paddle1) || ball.overlaps(paddle2)) {
 				ballVelocity.x = -ballVelocity.x;
-				// augmenter la vitesse de la balle de 10% à chaque impact
 				ballVelocity.scl(1.06f);
+
 			}
 
-			// Contrôle des palettes avec les touches du clavier
-			if (Gdx.input.isKeyPressed(Input.Keys.W) && paddle1.y + paddle1.height < 480) {
+			if (Gdx.input.isKeyPressed(Input.Keys.W) && paddle1.y + paddle1.height < GAME_HEIGHT) {
 				paddle1.y += 5;
 			}
 
@@ -64,7 +82,7 @@ public class MyGdxGame extends ApplicationAdapter {
 				paddle1.y -= 5;
 			}
 
-			if (Gdx.input.isKeyPressed(Input.Keys.UP) && paddle2.y + paddle2.height < 480) {
+			if (Gdx.input.isKeyPressed(Input.Keys.UP) && paddle2.y + paddle2.height < GAME_HEIGHT) {
 				paddle2.y += 5;
 			}
 
@@ -72,32 +90,56 @@ public class MyGdxGame extends ApplicationAdapter {
 				paddle2.y -= 5;
 			}
 
-			// Si la balle sort de l'écran à droite ou à gauche, on la supprime et on lance une nouvelle balle avec espace
-			if (ball.x + ball.width < 0 || ball.x > Gdx.graphics.getWidth()) {
-				int screenWidth = Gdx.graphics.getWidth();
-				int screenHeight = Gdx.graphics.getHeight();
-				System.out.println(" ball.width : " +  ball.width);
-				System.out.println("ball.x : " + ball.x);
-				System.out.println("largeur : " + Gdx.graphics.getWidth());
-				System.out.println("hauteur : " + Gdx.graphics.getHeight());
-				System.out.println("paddle2.x : " + paddle2.x);
-
+			if (ball.x + ball.width < 0 ) {
 				ballActive = false;
+				scorePlayer1 ++;
 			}
-		} else {
-			if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-				// on lance une nouvelle balle avec une nouvelle position et une vitesse aléatoire plus rapide
-				ball = new Rectangle(Gdx.graphics.getWidth() / 2 - 5, Gdx.graphics.getHeight() / 2 - 5, 10, 10);
-				ballVelocity = getRandomVelocity(120); // augmenter la valeur pour une vitesse initiale plus rapide
-				ballActive = true;
+			if (ball.x > Gdx.graphics.getWidth()) {
+				ballActive = false;
+				scorePlayer2 ++;
 			}
 		}
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) || spaceButton.contains(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY())
+				&& Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+			ballActive = false;
+			ball = new Rectangle(Gdx.graphics.getWidth() / 2 - 5, GAME_HEIGHT / 2 - 5, 10, 10);
+			ballVelocity = getRandomVelocity(120);
+			ballActive = true;
+		}
+
+		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+			int screenX = Gdx.input.getX();
+			int screenY = Gdx.input.getY();
+			int y = Gdx.graphics.getHeight() - screenY; // invert Y coordinate
+			if (resetButton.contains(screenX, y)) {
+				scorePlayer1 = 0;
+				scorePlayer2 = 0;
+				Gdx.app.log("InputProcessor", "Reset button clicked");
+			}
+			else if (creditsButton.contains(screenX, y)) {
+				System.out.println("Jeu créé par Clément Duval");
+				Gdx.app.log("InputProcessor", "Credits button clicked");
+			}
+			else if (spaceButton.contains(screenX, y)) {
+				// action pour le bouton dte lancement de balle
+				ballActive = false;
+				ball = new Rectangle(Gdx.graphics.getWidth() / 2 - 5, GAME_HEIGHT / 2 - 5, 10, 10);
+				ballVelocity = getRandomVelocity(120);
+				ballActive = true;
+				Gdx.app.log("InputProcessor", "Launch ball button clicked");
+			}
+			else if (quitButton.contains(screenX, y)) {
+				// action pour le bouton de quitter
+				Gdx.app.log("InputProcessor", "Quit button clicked");
+				Gdx.app.exit();
+			}
+		}
+
 	}
 
-
-
-
 	private void draw() {
+
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -111,7 +153,39 @@ public class MyGdxGame extends ApplicationAdapter {
 		shapeRenderer.rect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 		shapeRenderer.rect(ball.x, ball.y, ball.width, ball.height);
 
+		// dessiner la ligne blanche en haut et en bas
+		shapeRenderer.rect(0, BUTTON_HEIGHT+1, Gdx.graphics.getWidth(), 9);
+		shapeRenderer.rect(0, 470, Gdx.graphics.getWidth(), 9);
 		shapeRenderer.end();
+
+		// dessiner le score en haut de l'écran
+		SpriteBatch batch = new SpriteBatch();
+		batch.begin();
+		font.draw(batch, "Score joueur 1: " + scorePlayer1, 20, GAME_HEIGHT -20);
+		font.draw(batch, "Score joueur 2: " + scorePlayer2, Gdx.graphics.getWidth() - 200, GAME_HEIGHT -20);
+		batch.end();
+
+		//position curseur
+		batch.begin();
+		font.draw(batch, "X: " + Gdx.input.getX() + " Y: " + (Gdx.graphics.getHeight() - Gdx.input.getY()), Gdx.graphics.getWidth() - 450, GAME_HEIGHT - 20);
+		batch.end();
+
+		// dessiner les boutons du menu
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+		shapeRenderer.setColor(0.2f, 0.2f, 0.2f, 1);
+		shapeRenderer.rect(resetButton.x, resetButton.y, resetButton.width, resetButton.height);
+		shapeRenderer.rect(creditsButton.x, creditsButton.y, creditsButton.width, creditsButton.height);
+		shapeRenderer.rect(spaceButton.x, spaceButton.y, spaceButton.width, spaceButton.height);
+		shapeRenderer.rect(quitButton.x, quitButton.y, quitButton.width, quitButton.height);
+		shapeRenderer.end();
+
+		// dessiner le texte des boutons
+		batch.begin();
+		font.draw(batch, "Réinitialiser le jeu", resetButton.x + 20, resetButton.y + BUTTON_HEIGHT / 1.6f );
+		font.draw(batch, "Crédits", creditsButton.x + 80, creditsButton.y + BUTTON_HEIGHT / 1.6f );
+		font.draw(batch, "Lancer une nouvelle balle", spaceButton.x + 20, spaceButton.y + BUTTON_HEIGHT / 1.6f );
+		font.draw(batch, "Quitter le jeu", quitButton.x + 60, quitButton.y + BUTTON_HEIGHT / 1.6f );
+		batch.end();
 	}
 
 	private Vector2 getRandomVelocity(float speed) {
@@ -124,5 +198,8 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		shapeRenderer.dispose();
+		font.dispose();
 	}
+
+
 }
---
+++ core/src/com/mygdx/game/MyInputProcessor.java
@@ -0,0 +1,60 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+
+import java.awt.*;
+
+public class MyInputProcessor implements InputProcessor {
+
+    private Rectangle resetButton, creditsButton, spaceButton, quitButton;
+
+    public MyInputProcessor(Rectangle resetButton, Rectangle creditsButton, Rectangle spaceButton, Rectangle quitButton) {
+        this.resetButton = resetButton;
+        this.creditsButton = creditsButton;
+        this.spaceButton = spaceButton;
+        this.quitButton = quitButton;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+
+}
